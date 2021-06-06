d23b85a9332b3dff238d48f1970c3f2249125216
Olga <54333583+OlgaTsar@users.noreply.github.com>
2021-06-07 00:09:42 +0300

    Update HomeWork2.java

--- HomeWork2.java
+++ HomeWork2.java
@@ -1,2 +1,51 @@
-public class HomeWork2 {
+package Lesson1;
+
+public class HomeWork1 {
+
+
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign ();
+        printColor ();
+        compareNumbers ();
+    }
+    public static void printThreeWords() {
+        System.out.println("_Orange" + "\n" + "_Banana" + "\n" + "_Apple");
+    }
+
+    public static void checkSumSign () {
+        int a = 20;
+        int b = -30;
+        int c = a + b;
+
+        if (c > 0) {
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+
+    public static void printColor () {
+        int value = 101;
+        if (value <= 0) {
+            System.out.println("Красный");
+        }
+        if (value > 0 && value <= 100 ) {
+            System.out.println("Желтый");
+        }
+        else {
+            System.out.println("Зеленый");
+        }
+    }
+    public static void compareNumbers () {
+        int a = 6;
+        int b = 4;
+        if (a >= b){
+            System.out.println("a >= b");
+        }
+        else {
+            System.out.println("a < b");
+        }
+    }
 }
